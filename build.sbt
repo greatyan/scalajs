b824703949e110cae20d6d036ffaebea5981b36e
greatyan <greatyan@gmail.com>
2016-02-29 23:02:34 -0800

    Using DOM library

--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ lazy val root= (project in file(".")).
   settings(
     name := "hellojs",
     version := "1.0",
-    scalaVersion := "2.11.7"
+    scalaVersion := "2.11.7",
+    libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.0"
   ).
   enablePlugins(ScalaJSPlugin)
--- src/main/scala/HelloApp.scala
+++ src/main/scala/HelloApp.scala
@@ -1,7 +1,23 @@
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
+import org.scalajs.dom
+import dom.document
 
 object HelloApp extends JSApp {
   def main() : Unit = {
     println("Hello World!")
+    appendPar(document.body, "Hello World");
+  }
+
+  def appendPar(targetNode: dom.Node, text: String) {
+      val parNode = document.createElement("p");
+      val textNode = document.createTextNode(text);
+      parNode.appendChild( textNode);
+      targetNode.appendChild(parNode);
+  }
+
+  @JSExport
+  def addClickedMessage() {
+    appendPar(document.body, "Clicked");
   }
 }
